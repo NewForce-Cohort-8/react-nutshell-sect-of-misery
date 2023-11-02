b6cf44b2e79757f307d2a00c82970f14067902ff
Colin Matteson <seefm@proton.me>
2023-11-02 10:00:56 -0400

    finished login, register, and main page rendering, with some styling added

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,14 +1,9 @@
+
+
 export const ApplicationViews = () => {
 	
-    const localHoneyUser = localStorage.getItem("honey_user")
-    const honeyUserObject = JSON.parse(localHoneyUser)
+    // const localHoneyUser = localStorage.getItem("honey_user")
+    // const honeyUserObject = JSON.parse(localHoneyUser)
     
-    if (honeyUserObject) {
-
-        return <div>Application Views If</div>
-
-    }
-    else {
-        return <div>Application Views Else</div>
-    } 
+   
 }
